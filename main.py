f0fb90204bfc5701eb8ba0d225e4ef714a4c1d01
AdobeSpark <91537359+AdobeSpark@users.noreply.github.com>
2021-10-03 16:42:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,20 @@ import random
 
 
 # stuff for the request
-def codemain():
+def main():
 
-    profile_url = input('Enter your namemc URL: e.g: https://namemc.com/profile/Unpacks.5 ') # defining the url
-
-    with open('proxies.txt', 'r') as f:
-        proxies1 = f.read().splitlines()
-        proxy2 = random.choice(proxies1)
-        
-    proxies = {
-    'http': f'{proxy2}'
-    }
-
-
+    profile_url = input('Enter your NameMC URL >: ') # defining the url
+    
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
     headers = {'user-agent': f'{user_agent}'}
 
     if profile_url == profile_url:
-        r = requests.get(f'{profile_url}', proxies=proxies, headers=headers)
+        r = ProxyRequests(profile_url) 
+        r.set_headers(headers)
+        r.get_with_headers()
         time.sleep(10)
-        print(f'Pinged {profile_url} with proxy {proxy2} and status code {r.status_code}')
+        print(f'Pinged {profile_url} with proxy {r.get_proxy_used} and status code {r.get_status_code}')
 
-codemain()
+main()
 
 #cls to clr powershell
